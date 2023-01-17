3eb1ffb74440ebbcea76424eff6d4cf2c52e7788
rakibhasan47 <mdrakib6585@gmail.com>
2023-01-17 08:12:04 +0600

    module 27 js add

---
+++ variable.js
@@ -0,0 +1,13 @@
+// Number
+var watchPrice = 120;
+watchPrice = 125;
+// console.log(watchPrice);
+
+// String
+var userName = 'rakib hasan';
+
+// Boolean
+var hotelIsGood = true;
+
+// Integer 1 2 3 4 5 6 
+// Float 2.23 52.3 6.32
