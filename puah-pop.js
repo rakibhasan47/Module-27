70d2934391a55f9fb7b8b1a87053f8fdf6a7ba6d
rakibhasan47 <mdrakib6585@gmail.com>
2023-01-18 12:05:44 +0600

    js update an add js

---
+++ puah-pop.js
@@ -0,0 +1,40 @@
+
+// add push name ... // add - sanjida & adid //....
+var lastBanch = ['rakib', 'sakib', 'rifat', 'niha mim', 'shanta'];
+lastBanch.push('sanjida');
+lastBanch.push('adid');
+console.log(lastBanch);
+
+var friendsAge = [50, 30, 55, 21, 32];
+friendsAge.push(99);
+friendsAge.push(51);
+console.log(friendsAge);
+// End add push name ... // add - sanjida & adid //....
+
+            //........//
+
+// remove an element from an array //.......
+var friendsName = ['rakib', 'sakib', 'rifat', 'niha mim', 'shanta'];
+friendsName.pop();
+friendsName.pop();
+console.log(friendsName);
+
+var friendsYear = ['2002', '1999', '2001', '2004'];
+friendsYear.pop();
+friendsYear.pop();
+console.log(friendsYear);
+// remove an element from an array //.......
+
+
+          //.......//
+
+// remove an array first element //
+var cuntryName = ['bangladesh', 'india', 'malaysia', 'italy'];
+cuntryName.shift();
+console.log(cuntryName);
+// end remove an array first element //
+
+
+
+
+
